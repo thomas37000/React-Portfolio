073652104562a8d4287dd0b5af485bb3eb4ca92f
thomas <thomas.chalanson@sfr.fr>
2020-12-13 20:47:17 +0100

    css responsive description

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./map.css";
+import "./css/map.css";
 import L from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
